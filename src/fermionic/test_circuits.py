68f74e75cbc2f2443a2eeb804e1d3d87fa0a047e
BjornTCC <asbjornteilmann@gmail.com>
2026-03-23 09:33:34 +0100

    Need some functions outside test_circuits.py

--- src/fermionic/test_circuits.py
+++ src/fermionic/test_circuits.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy as sp
 
+from itertools import combinations
+
 from openfermion.ops import QubitOperator, FermionOperator
 from openfermion.linalg import get_sparse_operator
 from openfermion.transforms import jordan_wigner, normal_ordered
@@ -65,8 +67,6 @@ def compare_circuit_to_matrix(
                     print(format(i, f'0{nq}b') + "," + format(j, f'0{nq}b') + f": {circuit_matrix[i, j]}")
     return np.linalg.norm(check_matrix - circuit_matrix)
 
-from itertools import combinations
-
 def get_sorted_subsets_of_size(iterable, size):
   """
   Generates all sorted subsets (combinations) of a given size from an iterable.
@@ -90,42 +90,43 @@ op_part = FermionOperator("1^ 0^ 1 0", 1j)
 print(compare_circuit_to_matrix(op_part, theta, nq, False))
 """
 
-max_order = 4
-max_qubits = 4
-theta = np.pi/8
+if __name__ == "__main__":
+    max_order = 4
+    max_qubits = 4
+    theta = np.pi/8
 
-operator = FermionOperator("3^ 2^ 1 0")
-print(operator)
-print(compare_circuit_to_matrix(operator, theta, max_qubits, True))
-operator = FermionOperator("3^ 1^ 2 0")
-print(operator)
-print(compare_circuit_to_matrix(operator, theta, max_qubits, True))
+    operator = FermionOperator("3^ 2^ 1 0")
+    print(operator)
+    print(compare_circuit_to_matrix(operator, theta, max_qubits, True))
+    operator = FermionOperator("3^ 1^ 2 0")
+    print(operator)
+    print(compare_circuit_to_matrix(operator, theta, max_qubits, True))
 
-#exit()
-qubits_ind = list(range(max_qubits))
+    #exit()
+    qubits_ind = list(range(max_qubits))
 
-for order in range(1, max_order + 1):
-    if order % 2 != 0:
-        continue
-    for r,coeff in zip([True, False], [1.0, 1j]):
-        #for dec_order in [order // 2]:
-        for dec_order in range(order + 1):
-            inc_order = order - dec_order
-            inc_inds = list(get_sorted_subsets_of_size(qubits_ind, inc_order))
-            dec_inds = list(get_sorted_subsets_of_size(qubits_ind, dec_order))
-            for inc_ind in inc_inds:
-                for dec_ind in dec_inds:
-                    string = "".join([f"{i}^ " for i in inc_ind[::-1]]) + " " + "".join([f"{i} " for i in dec_ind[::-1]])
-                    op_part = FermionOperator(string, coeff)
-                    try:
-                        val = compare_circuit_to_matrix(op_part, theta, max_qubits, r, verbose=False)
+    for order in range(1, max_order + 1):
+        if order % 2 != 0:
+            continue
+        for r,coeff in zip([True, False], [1.0, 1j]):
+            #for dec_order in [order // 2]:
+            for dec_order in range(order + 1):
+                inc_order = order - dec_order
+                inc_inds = list(get_sorted_subsets_of_size(qubits_ind, inc_order))
+                dec_inds = list(get_sorted_subsets_of_size(qubits_ind, dec_order))
+                for inc_ind in inc_inds:
+                    for dec_ind in dec_inds:
+                        string = "".join([f"{i}^ " for i in inc_ind[::-1]]) + " " + "".join([f"{i} " for i in dec_ind[::-1]])
+                        op_part = FermionOperator(string, coeff)
+                        try:
+                            val = compare_circuit_to_matrix(op_part, theta, max_qubits, r, verbose=False)
 
-                        if val > 1e-12:
-                            print(f"Operator {op_part} has an error:")
-                            print(compare_circuit_to_matrix(op_part, theta, max_qubits, r, verbose=True))
+                            if val > 1e-12:
+                                print(f"Operator {op_part} has an error:")
+                                print(compare_circuit_to_matrix(op_part, theta, max_qubits, r, verbose=True))
+                                exit()
+
+                        except Exception as e:
+                            print(f"Operator {op_part} failed with: {e}")
                             exit()
-
-                    except Exception as e:
-                        print(f"Operator {op_part} failed with: {e}")
-                        exit()
-print("success")
+    print("success")
