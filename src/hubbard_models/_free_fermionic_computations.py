Fixed a factor 2 error in the freefermionic calculations in the hubbard model

src/hubbard_models/_free_fermionic_computations.py
@@ -24,7 +24,7 @@ def spectral_norm_of_free_fermionic_operator(
         data: T
 ) -> float:
     casted_data = cast_data_to_array(data)
-    return np.linalg.norm(casted_data, ord = 'nuc')
+    return np.linalg.norm(casted_data, ord = 'nuc') / 2
 
 @singledispatch
 def ff_commutator(op1: T, op2: T) -> T:

src/hubbard_models/split_operator_error_coefficients.py
@@ -4,7 +4,7 @@ import numpy as np
 from src.hubbard_models._free_fermionic_computations import spectral_norm_of_free_fermionic_operator
 
 def second_order_split_operator_error_coefficient(U: float, tau: float, N: int, hopping_graph: nx.Graph) -> float:
-    Hh = 2*tau * spectral_norm_of_free_fermionic_operator(hopping_graph)
+    Hh = 2*tau * 2*spectral_norm_of_free_fermionic_operator(hopping_graph)
     return min(
         (U**2 / 12) * Hh + (U * 4*tau**2 / 12) * N * (np.sqrt(5) + 8),
         (U * 4*tau**2 / 6) * N * (np.sqrt(5) + 8) + (U**2 / 24) * Hh