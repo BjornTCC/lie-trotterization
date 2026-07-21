ed2f295b69ec4188f88a73ef59868a0021d06317
BjornTCC <asbjornteilmann@gmail.com>
2026-07-21 15:20:58 +0200

    Updated hopping integral convention to be consistent with other litterature

--- src/hubbard_models/split_operator_error_coefficients.py
+++ src/hubbard_models/split_operator_error_coefficients.py
@@ -3,12 +3,29 @@ import numpy as np
 
 from src.hubbard_models._free_fermionic_computations import spectral_norm_of_free_fermionic_operator, ff_commutator
 
+def tight_binding_square_hopping_norm(tau: float, L: int) -> float:
+    ks = [2*np.pi *m / L for m in range(L)]
+    energies = [tau*abs(np.cos(kx) + np.cos(ky) + np.cos(kz)) for kx in ks for ky in ks for kz in ks]
+    return 2*sum(energies)
+
 def plaquette_second_order_split_operator_error_coefficient(U: float, tau: float, N: int, hopping_graph: nx.Graph) -> float:
-    Hh = 2*tau * 2*spectral_norm_of_free_fermionic_operator(hopping_graph)
+    Hh = tau * 2*spectral_norm_of_free_fermionic_operator(hopping_graph)
     return min(
-        (U**2 / 12) * Hh + (U * 4*tau**2 / 12) * N * (np.sqrt(5) + 8),
-        (U * 4*tau**2 / 6) * N * (np.sqrt(5) + 8) + (U**2 / 24) * Hh
+        (U**2 / 12) * Hh + (U * tau**2 / 12) * N * (np.sqrt(5) + 8),
+        (U * tau**2 / 6) * N * (np.sqrt(5) + 8) + (U**2 / 24) * Hh
     )
+
+def cubic_second_order_split_operator_error_coefficient(U: float, tau: float, L: int) -> float:
+    Hh = tight_binding_square_hopping_norm(tau, L)
+    BIHI = U ** 2 * Hh
+
+    N = L**3
+
+    _HHI_term = N*(6**2 * 4 + 2*24)
+
+    BHHI = (U / 2) * tau ** 2 * _HHI_term
+
+    return min(BHHI / 12 + BIHI / 24, BHHI / 24 + BIHI / 12)
 
 def second_order_split_operator_error_coefficient(U: float, tau: float, N: int, hopping_graph: nx.Graph) -> float:
     Hh = tau * 2*spectral_norm_of_free_fermionic_operator(hopping_graph)
@@ -25,7 +42,7 @@ def second_order_split_operator_error_coefficient(U: float, tau: float, N: int, 
                 else:
                     TI.add_edge(*edge, weight = 1.0)
 
-        _HHI_term += 2*spectral_norm_of_free_fermionic_operator(ff_commutator(TI, hopping_graph)) + 8*spectral_norm_of_free_fermionic_operator(TI)**2
+        _HHI_term += 2*spectral_norm_of_free_fermionic_operator(ff_commutator(TI, hopping_graph)) + 2*spectral_norm_of_free_fermionic_operator(TI)**2
 
     BHHI = (U / 2) * tau**2 * _HHI_term
 
@@ -33,20 +50,15 @@ def second_order_split_operator_error_coefficient(U: float, tau: float, N: int, 
 
 def fourth_order_suzuki_trotter_split_operator_error_coefficient(U: float, tau: float, N: int, d: int) -> float:
     return d*tau*U*N*min(
-        0.0568*U**3 + 14.0016*d*tau*U**2 + 21.4912*d**2*tau**2*U + 7.2192 * d**3*tau**3,
-        0.0094*U**3 + 5.5064*d*tau*U**2 + 28.6464*d**2*tau**2*U + 43.6224 * d**3*tau**3,
+        0.0284*U**3 + 3.5004*d*tau*U**2 + 2.6864*d**2*tau**2*U + 0.4512 * d**3*tau**3,
+        0.0047*U**3 + 1.3766*d*tau*U**2 + 3.5808*d**2*tau**2*U + 2.7264 * d**3*tau**3,
     )
-    #min(
-    #    0.9088*U**3 + 10.7008*d*tau*U**2 + 5.6256*d**2*tau**2*U + 1.2032 * d**3*tau**3,
-        #0.1504*U**3 + 7.6032*d*tau*U**2 + 5.5168*d**2*tau**2*U + 7.274 * d**3*tau**3
-    #)
 
 def fourth_order_augmented_split_operator_error_coefficients(U: float, tau: float, N: int, d: int, unitary_decomp: bool = True) -> tuple[float, float, float]:
-    #Wso = (4 / 15 * U**3 + 1453 / 320 * d*tau*U**2 + 36 / 5 * d**2 * tau**2 * U + 8 / 5 * d**3 * tau**3) * d*tau*U*N
-    Wso = (1 / 60 * U**3 + 9091 / 720 * d*tau*U**2 + 1028 / 45 * d**2 * tau**2 * U + 48 / 5 * d**3 * tau**3) * d*tau*U*N
-    WH2H1 = (5 / 9 * (d**2 + d) * tau + 2 / 3 * d**3 * tau + (5*d + 1) / 36 * U)*d*tau**2*U**2*N
-    WH2H2H1 = 1 / 72 * d**2 * tau**2 * U**4 * N
-    WSO7 = (67 / 672 * U + 440 / 21 * d * tau) * d**2*tau**2 * U**4 * N
+    Wso = (1 / 120 * U**3 + 9091 / 2880 * d*tau*U**2 + 257 / 90 * d**2 * tau**2 * U + 3 / 5 * d**3 * tau**3) * d*tau*U*N
+    WH2H1 = (5 / 72 * (d**3 + d**2) * tau + 1 / 12 * d * tau + (5*d + 1) / 144 * U)*d*tau**2*U**2*N
+    WH2H2H1 = 1 / 288 * d**2 * tau**2 * U**4 * N
+    WSO7 = (67 / 2688 * U + 55 / 21 * d * tau) * d**2*tau**2 * U**4 * N
     if unitary_decomp:
         return Wso + WH2H1, WH2H2H1, WSO7
     return Wso, WH2H2H1, WSO7
