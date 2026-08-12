467bc1e43bb004cb2963da96afc7f1d5ee1cc7bd
BjornTCC <asbjornteilmann@gmail.com>
2026-08-12 10:38:19 +0200

    Updated hexagonal hubbard model simulations, still work in progress

--- src/hubbard_models/hexagonal_hubbard.py
+++ src/hubbard_models/hexagonal_hubbard.py
@@ -7,7 +7,79 @@ import networkx as nx
 
 from scipy.optimize import fsolve, minimize_scalar
 from src.hubbard_models.free_fermionic_errors import error_between_exp_of_free_fermionic
-from src.hubbard_models._free_fermionic_computations import cast_data_to_array
+from src.hubbard_models._free_fermionic_computations import cast_data_to_array, spectral_norm_of_free_fermionic_operator
+from src.hubbard_models.split_operator_error_coefficients import fourth_order_augmented_split_operator_error_coefficients, fourth_order_suzuki_trotter_split_operator_error_coefficient
+
+def compute_number_of_trotter_steps(t: float, eps: float, U: float, tau: float, Lx: int, Ly: int, type: str) -> int:
+    if type == "tile":
+        W = second_order_error_coefficient(U, tau, Lx, Ly)
+        return math.ceil(
+            np.sqrt(W / eps) * t**(3/2)
+        )
+    elif type == "tile 4th":
+        W = fourth_order_suzuki_trotter_split_operator_error_coefficient(U, tau, 2*Lx*Ly,3)
+        #W = _fourth_order_suzuki_trotter_error_coefficient(U, tau, L)
+        return math.ceil(
+            (W / eps)**(1/4) * t**(5/4)
+        )
+    elif type == "augmented tile":
+        return _augmented_hexagonal_trotter_steps(t, eps, U, tau, Lx, Ly)
+    else:
+        raise ValueError(f"Invalid argument for type: {type}. Must be one of: \'plaquette\', \'plaquette suzuki-trotter\' or \'plaquette augmented\'.")
+
+def compute_evolution_time_and_number_of_simulation_circuits_for_qpe(eps: float, U: float, tau: float, Lx: int, Ly: int, type: str, unitary_decomp: bool = True) -> tuple[float,int]:
+    if type == "tile":
+        W = second_order_error_coefficient(U, tau, Lx, Ly)
+        return np.sqrt(eps / (3*W)), math.ceil(
+            6.203 * W**(1/2) / (eps**(3/2))
+        )
+    elif type == "tile 4th":
+        W = fourth_order_suzuki_trotter_split_operator_error_coefficient(U, tau, 2*Lx*Ly, 3)
+       # W = _fourth_order_suzuki_trotter_error_coefficient(U, tau, L)
+        return (eps / (5*W))**(1/4), math.ceil(
+            4.463 * W ** (1 / 4) /(eps ** (5 / 4))
+        )
+    elif type == "augmented tile":
+        return _augmented_plaquette_num_simulation_circuits(eps, U, tau, Lx, Ly, unitary_decomp = unitary_decomp)
+    else:
+        raise ValueError(f"Invalid argument for type: {type}. Must be one of: \'plaquette\', \'plaquette suzuki-trotter\' or \'plaquette augmented\'.")
+
+def second_order_error_coefficient(U: float, tau: float, Lx: int, Ly: int) -> float:
+    _, _, _, G = s3_tile_trotterization_graphs(Lx, Ly)
+    R1 = 2*spectral_norm_of_free_fermionic_operator(G)
+
+    return U*U*tau * R1 / 24 + 9.9*U*tau**2 * (2*Lx*Ly) / 12 + 0.8532 * tau**3 * (2*Lx*Ly)
+
+def _augmented_hexagonal_trotter_steps(t: float, eps: float, U: float, tau: float, Lx: int, Ly: int) -> int:
+    W5, W6, W7 = fourth_order_augmented_split_operator_error_coefficients(U, tau, 2*Lx*Ly, 3, unitary_decomp=True)
+
+    f = lambda n: W5 * t**5 / n**4 + W6 * t**6 / n**6 + W7 * t**7 / n**6 + n * _compute_augmented_trotter_error_numerically(t/n, tau, Lx, Ly) - eps
+
+    x0 = W5**(1/4) * t**(5/4) /(eps**(1/4))
+    res = fsolve(f, x0, full_output = True)
+    root = res[0][0]
+
+    return math.ceil(root)
+
+def _augmented_plaquette_num_simulation_circuits(eps: float, U: float, tau: float, Lx: int, Ly: int, unitary_decomp: bool = True) -> tuple[float, int]:
+    W5, W6, W7 = fourth_order_augmented_split_operator_error_coefficients(U, tau, 2*Lx*Ly, 3, unitary_decomp=unitary_decomp)
+    f = lambda t: W5 * t**5 + W6 * t**6 + W7 * t**7 + _compute_augmented_trotter_error_numerically(t, tau, Lx, Ly)
+
+    opt_func = lambda t: 0.76*np.pi/(t*eps - f(t))
+
+    t0 = (eps / (5*W5))**(1/4)
+    tm = (eps / W5)**(1/4)
+
+    bnds = [(0.00001,tm)]# These bounds ensure Npe > 0
+
+    min_res = minimize_scalar(opt_func, bounds = bnds[0])
+
+    if not min_res.success:
+        warnings.warn(f"Npe optimizer failed with message: {min_res.message}")
+
+    Npe = min_res.fun
+
+    return  min_res.x, math.ceil(Npe)
 
 def s3_tile_trotterization_graphs(Lx: int, Ly: int) -> tuple[nx.Graph, ...]:
     if (Ly % 2):
@@ -70,6 +142,7 @@ def hexagonal_3_correctors(Lx: int, Ly: int) -> tuple[nx.DiGraph, ...]:
     Gyr.add_edges_from([x[::-1] for x in edge_groups[2] if (x[0] in whole.nodes) and (x[1] in whole.nodes)])
     Gyb.add_edges_from([x for x in edge_groups[1] if (x[0] in whole.nodes) and (x[1] in whole.nodes)])
     return Gbr, Gyr, Gyb
+
 def hexagonal_3_correctors_decomposed(Lx: int, Ly: int) -> tuple[list[nx.DiGraph, ...], ...]:
     if (Ly % 2):
         raise ValueError(f"Only even values of Ly are allowed, got: {Ly}")
---
+++ src/resource_estimates/hexagonal_hubbard_model.py
@@ -0,0 +1,204 @@
+import math
+
+from src.hubbard_models.hexagonal_hubbard import compute_evolution_time_and_number_of_simulation_circuits_for_qpe, compute_number_of_trotter_steps
+
+from src.resource_estimates.gate_costs.protocol import ResourceGate
+from src.resource_estimates.gate_costs.free_fermionic import FreeFermionicS1Tile, FreeFermionicS2Tile, FreeFermionicS3Tile
+from src.resource_estimates.gate_costs.interactions import ShiftedOccupationPair
+from src.resource_estimates.gate_costs.hubbard_commutators import (
+    SpinSymmetricMixedControlledKappa,
+    SpinSymmetricMixedControlledHappa
+)
+from src.resource_estimates.gate_costs.hamming_weight_phasing import HWPGate
+from src.resource_estimates.gate_costs.parallelized_gate import ParallelizedResourceGate
+
+from src.resource_estimates.quantum_phase_estimation import (
+    quantum_phase_estimation_resources,
+    adaptive_phase_estimation_resources,
+)
+from src.resource_estimates.hamiltonian_simulation import (
+    hamiltonian_simulation_cost
+)
+
+import warnings
+
+def hubbard_model_phase_estimation_resources(
+        U: float,
+        tau: float,
+        Lx: int,
+        Ly: int,
+        target_error: float,
+        phase_estimation_algorithm: str = "adaptive",
+        time_evolution_algorithm: str = "tile",
+        x: float | None = None,
+        synthesize_rotation_with: str = "RUS",
+        hwp: bool = False
+) -> dict[str: int]:
+    if time_evolution_algorithm == "qubitization":
+        return get_qubitization_gate_counts(U, tau, Lx, Ly, target_error)
+    _,num_simulation_steps = compute_evolution_time_and_number_of_simulation_circuits_for_qpe(
+        target_error if x is None else target_error * (1 - x),
+        U,
+        tau,
+        Lx,
+        Ly,
+        time_evolution_algorithm,
+        unitary_decomp=False
+    )
+    trotter_step_gates, unitary_gates = get_qpe_gate_set(time_evolution_algorithm, Lx, Ly, hwp)
+
+    match phase_estimation_algorithm:
+        case "adaptive":
+            return adaptive_phase_estimation_resources(
+                target_error,
+                trotter_step_gates,
+                unitary_gates,
+                num_simulation_steps=num_simulation_steps,
+                x=x,
+                synthesize_rotation_with=synthesize_rotation_with
+            )
+
+        case "QPE":
+            return quantum_phase_estimation_resources(
+                target_error,
+                trotter_step_gates,
+                unitary_gates,
+                num_simulation_steps=num_simulation_steps,
+                x=x,
+                synthesize_rotation_with=synthesize_rotation_with
+            )
+        case _:
+            raise ValueError(f"phase_estimation_algorithm \"{phase_estimation_algorithm}\" not recognized/implemented")
+
+def hubbard_model_time_evolution_resources(
+        U: float,
+        tau: float,
+        Lx: int,
+        Ly: int,
+        t: float,
+        target_error: float,
+        algorithm: str = "tile",
+        x: float | None = None,
+        synthesize_rotation_with: str = "RUS",
+        hwp: bool = False
+) -> dict[str: int]:
+    num_simulation_steps = compute_number_of_trotter_steps(
+        t,
+        target_error if x is None else target_error * (1 - x),
+        U,
+        tau,
+        Lx,
+        Ly,
+        algorithm
+    )
+    trotter_step_gates, unitary_gates = get_time_evolution_gate_set(algorithm, Lx, Ly, hwp)
+
+    return hamiltonian_simulation_cost(
+        t,
+        target_error,
+        trotter_step_gates,
+        unitary_gates,
+        num_trotter_steps=num_simulation_steps,
+        x=x,
+        synthesize_rotation_with=synthesize_rotation_with
+    )
+
+def get_qpe_gate_set(type: str, Lx: int, Ly: int, hwp: bool) -> tuple[dict[ResourceGate: int], ...]:
+    N = 2*Lx*Ly
+    match type:
+        case "tile":
+            if hwp:
+                trotter_step_gates = {
+                    HWPGate(
+                        {FreeFermionicS2Tile(): N // 2},
+                        N
+                    ): 5,
+                    HWPGate({ShiftedOccupationPair(): N}, N): 1
+                }
+                unitary_gates = {
+                    HWPGate({ShiftedOccupationPair(): N}, N): 1
+                }
+            else:
+                trotter_step_gates = {
+                    ParallelizedResourceGate(
+                        {FreeFermionicS2Tile(): N // 2}
+                    ): 5,
+                    ParallelizedResourceGate({ShiftedOccupationPair(): N}): 1
+                }
+                unitary_gates = {
+                    ParallelizedResourceGate({ShiftedOccupationPair(): N}): 1
+                }
+        case "tile 4th":
+            if hwp:
+                trotter_step_gates = {
+                    HWPGate(
+                        {FreeFermionicS2Tile(): N // 2},
+                        N
+                    ): 5*5,
+                    HWPGate({ShiftedOccupationPair(): N}, N): 5
+                }
+                unitary_gates = {
+                    HWPGate({ShiftedOccupationPair(): N}, N): 1
+                }
+            else:
+                trotter_step_gates = {
+                    ParallelizedResourceGate(
+                        {FreeFermionicS2Tile(): N // 2}
+                    ): 5*5,
+                    ParallelizedResourceGate({ShiftedOccupationPair(): N}): 5
+                }
+                unitary_gates = {
+                    ParallelizedResourceGate({ShiftedOccupationPair(): N}): 1
+                }
+        case "augmented tile":
+            if hwp:
+                trotter_step_gates = {
+                    HWPGate(
+                        {FreeFermionicS3Tile(): N // 3},
+                        N
+                    ): 17,
+                    HWPGate({ShiftedOccupationPair(): N}, N): 2,
+                    HWPGate({SpinSymmetricMixedControlledHappa(): N // 2}, N // 2): 3,
+                }
+                unitary_gates = {
+                    HWPGate(
+                        {FreeFermionicS3Tile(): N // 3},
+                        N
+                    ): 17,
+                    HWPGate({SpinSymmetricMixedControlledKappa(): N // 2}, N // 2): 3,
+                }
+            else:
+                trotter_step_gates = {
+                    ParallelizedResourceGate(
+                        {FreeFermionicS3Tile(): N // 3}
+                    ): 17,
+                    ParallelizedResourceGate({ShiftedOccupationPair(): N}): 2,
+                    ParallelizedResourceGate({SpinSymmetricMixedControlledHappa(): N // 2}): 3,
+                }
+                unitary_gates = {
+                    ParallelizedResourceGate(
+                        {FreeFermionicS3Tile(): N // 3}
+                    ): 17,
+                    ParallelizedResourceGate({SpinSymmetricMixedControlledKappa(): N // 2}): 3,
+                }
+        case _:
+            raise ValueError(f"Unrecognized time evolution algorithm: {type}")
+
+    return trotter_step_gates, unitary_gates
+
+def get_time_evolution_gate_set(type: str, Lx: int, Ly: int, hwp: bool) -> tuple[dict[ResourceGate: int], ...]:
+    res1, res2 = get_qpe_gate_set(type, Lx, Ly, hwp)
+    if type == "augmented tile":
+        for x in res2.keys():
+            res2[x] *= 2
+    return res1, res2
+
+def get_qubitization_gate_counts(U: float, tau: float, Lx: int, Ly: int, target_error: float) -> dict[str: int]:
+    warnings.warn(
+        "For hexagonal hubbard model using qubitization, only T gate counts are available"
+    )
+    Cs = 40*Lx*Ly - 4
+    Cp = 0 # This contains only polylog factors that we neglect, much like square model.
+    Cr = 0
+    lambd = (3*tau + U/4)*2*Lx*Ly
+    return math.ceil(lambd*math.pi/(2*target_error)) * (Cs + 2*Cp + Cr)
